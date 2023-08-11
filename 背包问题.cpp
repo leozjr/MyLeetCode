fa9833e1384eca9f4556a0f414a412acae3a2d46
leo-Yoga13s <2684568332@qq.com>
2023-08-11 11:59:14 +0800

    背包问题

---
+++ 背包问题.cpp
@@ -0,0 +1,24 @@
+#include "LeetCodeUtils.h"
+
+/*
+有n件物品和一个最多能背重量为w 的背包。第i件物品的重量是weight[i]，得到的价值是value[i] 。每件物品只能用一次，求解将哪些物品装入背包里物品价值总和最大。
+*/
+
+class Solution {
+public:
+    int bagProblem(vector<int>& weight, vector<int> value){
+        return 0;
+    }
+
+    
+};
+
+int main(){
+    Solution s;
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+
+    cout << s.bagProblem(weight, value) << endl;
+
+    return 0;
+}
