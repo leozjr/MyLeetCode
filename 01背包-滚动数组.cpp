b664e3ed0fbda245f12c378827e16e5edb7a98b3
leo-Yoga13s <2684568332@qq.com>
2023-08-17 09:48:09 +0800

    背包问题-滚动数组

---
+++ 01背包-滚动数组.cpp
@@ -0,0 +1,25 @@
+#include "LeetCodeUtils.h"
+class Solution {
+public:
+    int bagProblem1D(vector<int>& weight, vector<int> value, int bag_weight){
+        vector<int> dp(bag_weight+1, 0);
+
+        for (int i = 0; i < weight.size(); i++){
+            for (int j = bag_weight; j >= weight[i]; j--){ // 注意遍历到 weight[i] 就停，因为 j 再小的话 i 是装不进去的，再继续遍历就错了。
+                dp[j] = max(dp[j-weight[i]] + value[i], dp[j]);
+            }
+        }
+        printVector(dp);
+        return dp[bag_weight];
+    }    
+};
+
+int main(){
+    Solution s;
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bag_weight = 4;
+    cout << s.bagProblem1D(weight, value, bag_weight) << endl;
+
+    return 0;
+}
